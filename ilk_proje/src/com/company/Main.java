3010faef89b5e9c0a007cb4edb659baa1fdb1bf2
Tamer Karalürt <tamerkaralurt@gmail.com>
2019-10-07 02:30:52 +0300

    Açıklamalar girildi

--- ilk_proje/src/com/company/Main.java
+++ ilk_proje/src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Main {
 
+    /**
+     * "psvm" ile direk main metodunu hızlı bir şekilde oluşturabiliriz.
+     * */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
