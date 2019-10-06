b8b251a42cd415be3a0705938f0a5b86f8cc97ca
Tamer Karalürt <tamerkaralurt@gmail.com>
2019-10-07 02:45:00 +0300

    Açıklama alanları girildi

--- ilk_proje/src/com/company/Main.java
+++ ilk_proje/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     /**
      * "psvm" ile direk main metodunu hızlı bir şekilde oluşturabiliriz.
+     * "sout" ile de System.out.printLn(""); hızlı bir şekilde oluşturulur.
+     * "\n" bir alt satıra geç komutu
      * */
     public static void main(String[] args) {
         System.out.println("Hello World!");
